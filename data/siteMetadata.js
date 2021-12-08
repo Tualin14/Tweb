ef212c5ac0d606d71571cc9f24176848fcfa4420
dzaaaaaa <dzaaaaaa@outlook.com>
2021-12-08 21:15:03 +0800

    giscus

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -33,9 +33,9 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+      repo: "Tualin14/Tweb",
       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+      category: "Announcements",
       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
       mapping: "pathname", // supported options: pathname, url, title
       reactions: "1", // Emoji reactions: 1 = enable / 0 = disable
