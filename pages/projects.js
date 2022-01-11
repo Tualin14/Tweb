35a8578d11c08b9c0a81f766ccc72d76524d1d40
dzaaaaaa <dzaaaaaa@outlook.com>
2022-01-11 23:35:42 +0800

    pj

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,7 @@
-import siteMetadata from '@/data/siteMetadata'
-import projectsData from '@/data/projectsData'
-import Card from '@/components/Card'
-import { PageSEO } from '@/components/SEO'
+import siteMetadata from "@/data/siteMetadata";
+import projectsData from "@/data/projectsData";
+import Card from "@/components/Card";
+import { PageSEO } from "@/components/SEO";
 
 export default function Projects() {
   return (
@@ -9,10 +9,11 @@ export default function Projects() {
       <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h1
+            className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Projects
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">这里是一段描述</p>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">君子生非异也，善假于物也。</p>
         </div>
         <div className="container py-12">
           <div className="flex flex-wrap -m-4">
@@ -29,5 +30,5 @@ export default function Projects() {
         </div>
       </div>
     </>
-  )
+  );
 }
