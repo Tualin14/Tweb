1e24e47ab7c38c77986ba3c6fb1411caf212f00e
dzaaaaaa <71857734+Tualin14@users.noreply.github.com>
2022-02-13 01:40:45 +0800

    1

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,8 +1,8 @@
 const projectsData = [
   {
     title: "OneSS",
-    description: `Onedrive Sharing System`,
-    imgSrc: "/static/images/dzaaaaaa.png",
+    description: `让你的 Onedrive 能像 OSS 一样使用 | Make your Onedrive work like OSS(Object Storage Service)`,
+    imgSrc: "https://oness.dzaaaaaa.com/oness.svg",
     href: "https://oness.dzaaaaaa.com"
   }
 ];
