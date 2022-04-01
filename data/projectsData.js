5cc72799c22a410bcaea2ed78520a78863de3750
Tualin14 <t@dzaaaaaa.com>
2022-04-01 14:25:31 +0800

    fix

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,7 +2,7 @@ const projectsData = [
   {
     title: "OneSS",
     description: `让你的 Onedrive 能像 OSS 一样使用 | Make your Onedrive work like OSS(Object Storage Service)`,
-    imgSrc: "https://oness.dzaaaaaa.com/OneSS_T.svg",
+    imgSrc: "https://oness.dzaaaaaa.com/OneSS.svg",
     href: "https://oness.dzaaaaaa.com"
   }
 ];
