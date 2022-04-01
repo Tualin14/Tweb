b206ed0a4e855867b20b0f3ec3c47d18b65d73f5
Tualin14 <t@dzaaaaaa.com>
2022-04-01 14:20:24 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
+  images: {
+    domains: ['oness.dzaaaaaa.com'],
+  },
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   eslint: {
